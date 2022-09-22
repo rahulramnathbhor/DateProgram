07964d54c4bf400ba6d3abe9b603a1ecda97093b
rahulramnathbhor <112869809+rahulramnathbhor@users.noreply.github.com>
2022-09-22 13:41:16 +0530

    Add project files.

---
+++ DateProgram/Date.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateProgram
+{
+    internal class Date1
+    {
+        int day, year;
+        string month;
+
+        public void AssignValue()
+        {
+            day = 14;
+            month = "sep";
+            year = 2022;
+        }
+
+        public string DisplayValue()
+        {
+            return $"Date is :{day} / {month} / {year}";
+        }
+
+
+
+
+
+    }
+}
---
+++ DateProgram/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Date1 date1 = new Date1();
+            date1.AssignValue();
+            Console.WriteLine(date1.DisplayValue());
+        }
+    }
+}
